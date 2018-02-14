1ac07a4f9cf777627f34ca68c75d7fa9c60848b8
David Bell <dbell@MacBook-Pro.local>
2018-02-14 12:05:18 -0500

    Chapter 1 updates

---
+++ CH1_Declarations_AccessControl/LegalIdentifiers/Valid.java
@@ -0,0 +1,9 @@
+class Valid {
+    public static void main(String[] args) {
+        int _a;
+        int $c;
+        int ________2_w;
+        int _$;
+        int this_is_a_very_long_identifier;
+    }
+}
