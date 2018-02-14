b389938c33c0b04c4d727e5ec0ebe97187eea46a
David Bell <dtbell99@gmail.com>
2018-02-13 21:44:53 -0500

    First Push

---
+++ Chapter1/HelloWorld.java
@@ -0,0 +1,10 @@
+import java.util.Date;
+
+class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Today is " + new Date());
+    }
+
+}
