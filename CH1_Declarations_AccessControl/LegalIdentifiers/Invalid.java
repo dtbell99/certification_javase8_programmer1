6329eb90977968bcb63927ca5df330160847641e
David Bell <dbell@MacBook-Pro.local>
2018-02-14 14:42:02 -0500

    Invalid options

---
+++ CH1_Declarations_AccessControl/LegalIdentifiers/Invalid.java
@@ -0,0 +1,9 @@
+class Invalid {
+    public static void main(String[] args) {
+        int 1variable = 0;
+        int -variable = 0;
+        int +variable = 0;
+        int e#;
+        int .f;
+    }
+}
